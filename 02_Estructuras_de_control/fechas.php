f38a6d8ce446c65c898c92135f732f8327e2feec
justmanuva <manuva2662004@gmail.com>
2024-09-25 10:57:44 +0200

    ejercicios clase dia 25-09

---
+++ 02_Estructuras_de_control/fechas.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fechas</title>
+</head>
+<body>
+    <?php
+    $numero = "2";
+    $numero = (int) $numero;
+
+    if($numero === 2){
+        echo "EXITO";
+    } else {
+        echo "NO EXITO";
+    }
+
+    /*
+        "2" == 2    "2" es igual a 2
+        "2" !== 2   "2" no es idéntico a 2
+        2 === 2     2 sí es idéntico a 2
+        2 !== 2.0   2 no es idéntico a 2.0
+    */
+
+    $hora = (int)date("G");
+    var_dump($hora);
+    
+    /*
+        SI $hora ENTRE 6 y 11, es MAÑANA
+        SI $hora ENTRE 12 y 14, es MEDIODÍA
+        SI $hora ENTRE 15 y 20, es TARDE
+        SI $hora ENTRE 20 y 0, es NOCHE
+        SI $hora ENTRE 1 y 5, es MADRUGADA
+    */
+
+    if ($hora >= 6 and $hora <= 11) echo "es MAÑANA";
+    elseif ($hora >= 12 and $hora <= 14) echo "es MEDIODÍA";
+    elseif ($hora >= 15 and $hora <= 20) echo "es TARDE";
+    elseif ($hora >= 20 and $hora <= 0) echo "es NOCHE";
+    elseif ($hora >= 1 and $hora <= 5) echo "es MADRUGADA";
+
+    $hora_exacta = date("H:i:s");
+
+    echo "<h1>$hora_exacta</h1>";
+
+    $dia = date("l");
+    echo "<h2>Hoy es $dia</h2>";
+
+    /*
+        TENEMOS CLASE LUNES, MIÉRCOLES Y VIERNES
+        NO TENEMOS CLASE EL RESTO
+
+        HACER UN SWITCH QUE DIGA SI HOY TENEMOS CLASE
+    */
+
+    switch($dia){
+        case "Monday":
+            echo "<p>Hoy tenemos clase</p>";
+            break;
+        case "Wednesday":
+            echo "<p>Hoy tenemos clase</p>";
+            break;
+        case "Friday":
+            echo "<p>Hoy tenemos clase</p>";
+            break;
+        default:
+            echo "<p>Hoy NO tenemos clase</p>";
+    }
+
+    ?>
+</body>
+</html>
---
+++ 02_Estructuras_de_control/numeros.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Números</title>
+</head>
+<body>
+    <?php
+    $numero = 0;    
+    /*
+    //Forma 1
+    if($numero > 0){
+        echo "<p>1 El número $numero es mayor que cero</p>";
+    } elseif ($numero == 0){
+        echo "<p>1 El número $numero es cero</p>";
+    } else {
+        echo "<p>1 El número $numero es menor que cero</p>";
+    }
+
+    //Forma 2
+    if($numero > 0) echo "<p>2 El número $numero es mayor que cero</p>";
+    elseif ($numero == 0) echo "<p>2 El número $numero es cero</p>";
+    else echo "<p>2 El número $numero es menor que cero";
+
+    //Forma 3
+    if($numero > 0):
+        echo "<p>3 El número $numero es mayor que cero</p>";
+    elseif($numero == 0):
+        echo "<p>3 El número $numero es cero";
+    else:
+        echo "<p>3 El número $numero es menor que cero";
+    endif;
+    */
+    ?>
+
+    <?php
+    # Rangos [-10,0),[0,10],(10,20]
+
+    $num = -7;
+
+    # and o && para la conjunción
+
+    if($num >= -10 and $num <= 10) echo "<p>El número $num está en el rango [-10,0)";
+    if ($num >= -10 && $num < 0) echo "<p>1 El número $num está en el rango [";
+    
+    /*
+        COMPROBAR DE TRES FORMAS DIFERENTES, CON LA ESTRUCTURA IF, SI EL NÚMERO ALEATORIO
+        TIENE 1, 2 O R DÍGITOS
+    */
+    
+    
+    $numero_aleatorio = rand(1,200);
+    $digitos = null;
+
+    
+
+    //Forma 1
+    if ($numero_aleatorio < 10) echo "<p>$numero_aleatorio tiene 1 digito</p>";
+    elseif ($numero_aleatorio > 9 and $numero_aleatorio <100) echo "<p>$numero_aleatorio tiene 2 digitos</p>";
+    else echo "<p>$numero_aleatorio tiene 3 digitos</p>";
+
+    //Forma 2
+    if ($numero_aleatorio < 10):
+        echo "<p>$numero_aleatorio tiene 1 digito</p>";
+    elseif ($numero_aleatorio > 9 and $numero_aleatorio <100):
+        echo "<p>$numero_aleatorio tiene 2 digitos</p>";
+    else:
+        echo "<p>$numero_aleatorio tiene 3 digitos</p>";
+    endif;
+
+    //Forma 3 (Corregida)
+    if ($numero_aleatorio >= 1 and $numero_aleatorio < 10){
+        $digitos = 1;
+    } elseif ($numero_aleatorio > 9 and $numero_aleatorio <100){
+        $digitos = 2;
+    } elseif ($numero_aleatorio >= 100 and $numero_aleatorio <= 200) {
+        $digitos = 3;
+    } else {
+        $digitos = "ERROR";
+    }
+
+    $digitos_texto = "dígitos";
+    if($digitos == 1) $digitos_texto = "dígito";
+
+    echo "<p>El número $numero_aleatorio tiene $digitos dígitos</p>";
+
+    // $numero_aleatorio_decimales = rand(10,100)/10;
+    
+    $n = rand(1,3);
+
+    switch($n) {
+        case 1:
+            echo "El número es 1";
+            break;
+        case 2:
+            echo "El número es 2";
+            break;
+        default:
+            echo "El número es 3";
+            break;
+    }
+
+    ?>
+
+</body>
+</html>
