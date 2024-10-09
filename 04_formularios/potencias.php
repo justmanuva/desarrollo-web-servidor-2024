86a148d7fb2b10d3968374d08fd481d4c7e94c3d
justmanuva <manuva2662004@gmail.com>
2024-10-09 10:28:52 +0200

    04_formularios updated

--- 04_formularios/potencias.php
+++ 04_formularios/potencias.php
@@ -10,40 +10,44 @@
   ?>
 </head>
 <body>
-  <?php
-    /**
-     * CREAR UN FORMULARIO QUE RECIBA DOS PARÁMETROS: BASE Y EXPONENTE
-     * 
-     * CUANDO SE ENVÍE EL FORMULARIO, SE CALCULARÁ EL RESULTADO DE ELEVAR
-     * LA BASE AL EXPONENTE
-     * 
-     * EJEMPLOS:
-     * 
-     * 2 ELEVADO A 3 = 8 => 2X2X2 = 8
-     * 3 ELEVADO A 2 = 9 => 3X3 = 9
-     * 2 ELEVADO A 1 = 2
-     * 3 ELEVADO A 0 = 1
-     */
-  ?>
+    <form action="" method="post">
+      
+      <!-- for= "base" lo enlaza con el input Base para que al pinchar se seleccione el texto -->
+      <label for="base">Base</label>
+      <input type="text" name="base" id="base" placeholder="introduzca la base"><br><br>
+      <label for="exponente">Exponente</label>
+      <input type="text" name="exponente" id="exponente" placeholder="introduzca el exponente"><br><br>
+      <input type="submit" value="Calcular">
+    </form>
+  
+    <?php
+      /**
+       * CREAR UN FORMULARIO QUE RECIBA DOS PARÁMETROS: BASE Y EXPONENTE
+       * 
+       * CUANDO SE ENVÍE EL FORMULARIO, SE CALCULARÁ EL RESULTADO DE ELEVAR
+       * LA BASE AL EXPONENTE
+       * 
+       * EJEMPLOS:
+       * 
+       * 2 ELEVADO A 3 = 8 => 2X2X2 = 8
+       * 3 ELEVADO A 2 = 9 => 3X3 = 9
+       * 2 ELEVADO A 1 = 2
+       * 3 ELEVADO A 0 = 1
+       */
 
-  <form action="" method="post">
-    <input type="text" name="base">
-    <input type="text" name="exponente">
-    <input type="submit" value="Enviar">
-  </form>
+      if($_SERVER["REQUEST_METHOD"] == "POST") {
 
-  <?php
-    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $base = $_POST["base"];
+        $exponente = $_POST["exponente"];
+        $resultado = 1;
 
-      $base = $_POST["base"];
-      $exponente = $_POST["exponente"];
+        for($i = 0; $i < $exponente; $i++) {
+          $resultado *= $base;
 
-      $resultado = $base ** $exponente;
+        }
 
-      echo "<h1>$base ELEVADO A $exponente = $resultado</h1>";
-    }
-
-
-  ?>
+        echo "<h1>El resultado es $resultado</h1>";
+      }
+    ?>
 </body>
 </html>
