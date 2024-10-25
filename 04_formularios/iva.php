6087d6b7a29e63d7091f9318ca6b56e0d5b657b6
justmanuva <manuva2662004@gmail.com>
2024-10-25 20:05:10 +0200

    Mod IVA hoy (Falta completar)

--- 04_formularios/iva.php
+++ 04_formularios/iva.php
@@ -28,7 +28,9 @@
   <?php
     if($_SERVER["REQUEST_METHOD"] == "POST") {
       $tmp_precio = $_POST["precio"];
-      $tmp_iva = $_POST["iva"];
+      // Comprobamos esto para que tenga que meterte algo si o si y si no lo manda vacio
+      if (isset($_POST["iva"])) $tmp_iva = $_POST["iva"];
+      else $tmp_iva = "";
 
       if ($tmp_precio == "") echo "<p>El precio es obligatorio</p>";
       else {
@@ -49,10 +51,33 @@
           echo "<p>El IVA solo puede ser: GENERAL, REDUCIDO, SUPERREDUCIDO</p>";
         } else $iva = $tmp_iva;
       }
-      if(isset($precio) and isset($iva)){
-        echo calcularPVP($precio, $iva);
-      }
     }
   ?>
+  <!--
+    Ponemos el php arriba porque si no la variable err_precio no existiria, siemor ese recomienda ponerlo arriba
+    Aparte no influye porque la pagina cuando la abres entra por get, y cuando mandas el formulario se reinicia la pagina
+    y sale el post y ya ahi se ejecuta el codigo php.
+-->
+  <form action="" method="post">
+    <input type="text" name="Precio">
+    <?php if(isset($err_precio)) echo "<span class = 'error'>$err_precio</span>";?><!--si se ha producido algun error sale por pantalla-->
+    <br>
+    <select name="iva" id="iva">
+        <option disabled selected hidden>--- Elige un tipo de IVA ---</option><!-- Esto le pone un enunciado a lo de las opciones -->
+        <option value="General">General</option>
+        <option value="Reducido">Reducido</option>
+        <option value="Superreducido">Superreducido</option>
+    </select>
+    <?php if(isset($err_iva)) echo "<span class = 'error'>$err_iva</span>";?>
+    <br>
+    <input type="submit" value="Calcular">
+  </form>
+
+    <!-- Ponemos esto aqui para que nos salga el precio abajo y no arriba -->
+    <?php 
+        if(isset($tmp_precio) && isset($tmp_iva)) {
+            echo "<h1>El PVP es " . iva($tmp_precio, $tmp_iva) . "</h1>";
+        }
+    ?>
 </body>
 </html>
