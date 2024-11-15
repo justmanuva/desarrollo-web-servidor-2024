4fcfd960c4daae84c566420bc1686cad37b8c383
justmanuva <manuva2662004@gmail.com>
2024-11-15 12:58:12 +0100

    Todo listo para practica 2

--- 05_funciones/depurar.php
+++ 05_funciones/depurar.php
@@ -5,7 +5,7 @@
         // Para quitar los espacios de delante y detrás
         $salida = trim($salida);
         // Quita posibles bugs muy raros como que el usuario introduzca: \n (No está de más ponerla)
-        $salida = stripcslashes($salida);
+        $salida = stripslashes($salida);
         // Para quitar los múltiples espacios entre variables y demás
         $salida = preg_replace('!\s+!', ' ', $salida);
         return $salida;
