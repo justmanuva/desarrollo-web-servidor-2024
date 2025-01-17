45679a33a78e02a9b8fcef5c4a9cfbe28c4f49be
justmanuva <manuva2662004@gmail.com>
2025-01-17 09:33:48 +0100

    Subida temporal

--- Sucio/practica_escrito.php
+++ Sucio/practica_escrito.php
@@ -3,16 +3,14 @@
 		$id_anime = $_POST["id_anime"];
 
 		// DELETE
-		
 		$sql = $_conexion -> prepare("DELETE FROM animes WHERE id_anime = ?");
 		$sql -> bind_param("i", $id_anime);
 		$sql -> execute();
 
 		// INSERT
-
-		$sql = $_conexion -> prepare("INSERT INTO animes (titulo, nombre_estudio,
-			anno_estreno, num_temporadas, imagen)
-			VALUES(?,?,?,?,?)");
+		$sql = $_conexion -> prepare("INSERT INTO animes (titulo, nombre_estudio, anno_estreno,
+			num_temporadas, imagen)
+			VALUES (?,?,?,?,?)");
 		$sql -> bind_param("ssiis",
 			$titulo,
 			$nombre_estudio,
@@ -22,24 +20,20 @@
 		);
 		$sql -> execute();
 
-
-		// SELECT
-
-		$sql = $_conexion -> prepare("SELECT * FROM animes WHERE id_anime = ?");
+		// SELECT (Con parámetros)
+		$sql = $_conexion -> prepare("SELECT * FROM animes WHERE id_animes = ?");
 		$sql -> bind_param("i", $id_anime);
 		$sql -> execute();
 		$resultado = $sql -> get_result();
 
 		// UPDATE
-	
-		$sql = $_conexion ->prepare("UPDATE animes SET 
+		$sql = $_conexion -> prepare("UPDATE animes SET
 			titulo = ?,
 			nombre_estudio = ?,
 			anno_estreno = ?,
 			num_temporadas = ?,
 			WHERE id_anime = ?
 		");
-
 		$sql -> bind_param("ssiii",
 			$titulo,
 			$nombre_estudio,
@@ -47,72 +41,59 @@
 			$num_temporadas,
 			$id_anime
 		);
+		$sql -> execute();
 
-
+		// SELECT (Order by)
+		$sql = $_conexion -> prepare("SELECT * FROM animes ORDER BY ?");
+		$sql -> bind_param("i", $id_anime);
+		$sql -> execute();
+		$resultado = $sql -> get_result();
 	}
 
+
+	// SELECT (Sin parámetros)
 	$sql = "SELECT * FROM animes";
-	$resultado = $_conexion ->query($sql);
+	$resultado = $_conexion -> query($sql);
 
 	$_conexion -> close();
 ?>
-
 <h1>Tabla de animes</h1>
-
 <table>
 	<thead>
 		<tr>
-			<th>Titulo</th>
-			<th>Estudio</th>
-			<th>Año</th>
-			<th>Temporadas</th>
-			<th>Imagen</th>
-			<th></th>
-			<th></th>
+			<td>Título</td>
+			<td>Estudio</td>
+			<td>Año</td>
+			<td>Número de temporadas</td>
+			<td>Imagen</td>
+			<td></td>
+			<td></td>
 		</tr>
 	</thead>
 	<tbody>
-		<?php		
-		while ($fila = $resultado -> fetch_assoc()) {
-			echo "<tr>";
-			echo "<td>" . $fila["titulo"] . "</td>";
-			echo "<td>" . $fila["nombre_estudio"] . "</td>";
-			echo "<td>" . $fila["anno_estreno"] . "</td>";
-			echo "<td>" . $fila["num_temporadas"] . "</td>";
+		<?php
+			while($fila = $resultado -> fetch_assoc()) {
+				echo "<tr>";
+				echo "<td>" . $fila["titulo"] . "</td>";
+				echo "<td>" . $fila["nombre_estudio"] . "</td>";
+				echo "<td>" . $fila["anno_estreno"] . "</td>";
+				echo "<td>" . $fila["num_temporadas"] . "</td>";
+				?>
+				<td>
+					<img width=100 height=200 src="<?php echo $fila["imagen"] ?>">
+				</td>
+				<td>
+					<a href="ver_anime.php?id_anime=<?php echo $fila["id_anime"] ?>">Editar</a> 
+				</td>
+				<td>
+					<form action="" method="post">
+						<input type="hidden" name=id_anime value="<?php echo $fila["id_anime"] ?>">
+						<input type="submit" value="Borrar">
+					</form>
+				</td>
+				<?php
+				echo "</tr>";
+			}
 			?>
-			<td>
-				<img width=100 height=200 src="<?php echo $fila["imagen"] ?>">
-			</td>
-			<td>
-				<a href="ver_anime.php?id_anime=<?php echo $fila["id_anime"] ?>">Editar</a>
-			</td>
-			<td>
-				<form action="" method="post">
-					<input type="hidden" value="<?php echo $fila["id_anime"] ?>">
-					<input type="submit" value="Borrar">
-				</form>
-			</td>
-			<?php
-			echo "</tr>";
-		} ?>
 	</tbody>
 </table>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
