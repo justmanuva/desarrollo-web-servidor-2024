065505c14c5f9eee1f9f027b084eb4089818aed8
justmanuva <manuva2662004@gmail.com>
2025-01-27 13:59:12 +0100

    Subida temporal

--- 07_apis/jikan/anime.php
+++ 07_apis/jikan/anime.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Información del anime</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <?php
         error_reporting(E_ALL);
         ini_set("display_errors", 1);
@@ -12,7 +13,7 @@
 <body>
     <?php
         $id = $_GET["id"];
-        $apiUrl = "https://api.jikan.moe/v4/anime/$id";
+        $apiUrl = "https://api.jikan.moe/v4/anime/$id/full";
 
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $apiUrl);
@@ -24,6 +25,47 @@
         $anime = $datos["data"];
         /* print_r($anime) */
     ?>
-    <h1><?php echo $anime["title"] ?></h1>
+    <!-- 
+    MOSTRAR:
+    - titulo
+    - nota
+    - sinopsis
+    - lista de generos
+    - trailer (embed_id)
+    - lista de animes relacionados
+        + Solo el nombre del anime.
+        + Solo se mostrarán los relacionados que sean de type anime
+    -->
+
+    <div class="container">
+        <h1><?php echo $anime["title"] ?></h1>
+        <img src="<?php echo $anime["images"]["jpg"]["image_url"]?>"><br>
+        <h2>Nota media: <?php echo $anime["score"] ?></h2><br>
+        <h2>Sinopsis:</h2>
+        <p><?php echo $anime["synopsis"]?></p><br>
+        <h2>Géneros:</h2>
+        <ul class="list-group">
+            <?php foreach($anime["genres"] as $genero) {  ?>
+                <li class="list-group-item"><?php echo $genero["name"]?></li>
+            <?php } ?>
+        </ul><br>
+        <h2>Relacionados:</h2>
+        <ul class="list-group"  >
+            <?php foreach($anime["relations"] as $relacionado) {
+                    foreach($relacionado["entry"] as $entrada) {
+                        if ($entrada["type"] == "anime") { ?>
+                            <li class="list-group-item">
+                                <a href="anime.php?id=<?php echo $entrada["mal_id"]?>">
+                                    <?php echo $entrada["name"]?>
+                                </a>    
+                            </li>
+                    <?php }
+                    }
+                } ?>
+        </ul><br>
+        <iframe width="500px" height="350px" src="<?php echo $anime["trailer"]["embed_url"]?>"></iframe>
+
+    </div>
+    
 </body>
 </html>
--- 07_apis/jikan/top_anime.php
+++ 07_apis/jikan/top_anime.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Top Anime</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <?php
         error_reporting(E_ALL);
         ini_set("display_errors", 1);
@@ -22,28 +23,27 @@
         $datos = json_decode($respuesta, true);
         $animes = $datos["data"];
     ?>
-    <table>
-        <thead>
+    <table class="table">
+        <thead class="thead-dark">
             <tr>
-                <th>Posición</th>
-                <th>Título</th>
-                <th>Nota</th>
-                <th>Imagen</th>
-                <th>Trailer</th>
+                <th scope="col">Posición</th>
+                <th scope="col">Título</th>
+                <th scope="col">Nota</th>
+                <th scope="col">Imagen</th>
             </tr>
         </thead>
         <tbody>
             <?php
                 foreach($animes as $anime) { ?>
                     <tr>
-                        <td><?php echo $anime["rank"]?></td>
+                        <td scope="row"><?php echo $anime["rank"]?></td>
                         <td>
                             <a href="anime.php?id=<?php echo $anime["mal_id"]?>">
                                 <?php echo $anime["title"]?>
                             </a>
-                        </td>
-                        <td><?php echo $anime["score"]?></td>
-                        <td>
+                        </td scope="row">
+                        <td scope="row"><?php echo $anime["score"]?></td>
+                        <td scope="row">
                             <img width="100px" src="<?php echo $anime["images"]["jpg"]["image_url"]?>">
                         </td>
                     </tr>
