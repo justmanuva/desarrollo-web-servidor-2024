f187026b5f42d4852e5f7d81dfbf8380a345c404
justmanuva <manuva2662004@gmail.com>
2025-02-01 18:05:15 +0100

    Dog-api | random done

---
+++ 07_apis/dog-api/random.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perros random</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <?php
+        error_reporting(E_ALL);
+        ini_set("display_errors", 1);
+    ?>
+</head>
+<body>
+    <?php
+        $apiUrl = "https://dog.ceo/api/breeds/image/random";
+        
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $apiUrl);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $respuesta = curl_exec($curl);
+        curl_close($curl);
+
+        $datos = json_decode($respuesta, true);
+        $random_image = $datos["message"];
+    ?>
+    <img width="400px" src="<?php echo $random_image ?>"><br><br>
+    <a href="./random.php" class="btn btn-info">Aleatorio</a>
+
+</body>
+</html>
