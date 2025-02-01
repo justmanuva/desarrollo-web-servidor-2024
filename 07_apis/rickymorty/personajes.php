908f3650a8a3b4271b118e765e792c5c52259a18
justmanuva <manuva2662004@gmail.com>
2025-02-01 17:32:53 +0100

    Rick&Morty | Quantity/Pages implemented, rare action

--- 07_apis/rickymorty/personajes.php
+++ 07_apis/rickymorty/personajes.php
@@ -14,13 +14,13 @@
     <?php
         $apiUrl = "https://rickandmortyapi.com/api/character";
         
-        $cantidad = isset($_GET["page"]) ? $_GET["page"] : "";
+        $pagina_actual = isset($_GET["page"]) ? $_GET["page"] : "";
+        $cantidad = isset($_GET["quantity"]) ? $_GET["quantity"] : "";
         $genero = isset($_GET["gender"]) ? $_GET["gender"] : "";
         $especie = isset($_GET["species"]) ? $_GET["species"] : "";
         
-        
-        if (isset($_GET["gender"]) && isset($_GET["species"]) && isset($_GET["quantity"])) { 
-            $apiUrl = "https://rickandmortyapi.com/api/character?page=$cantidad&species=$especie&gender=$genero";
+        if (isset($_GET["gender"]) && isset($_GET["species"]) && isset($_GET["quantity"]) && isset($_GET["page"])) { 
+            $apiUrl = "https://rickandmortyapi.com/api/character?page=$pagina_actual&species=$especie&gender=$genero";
         }
 
         $curl = curl_init();
@@ -31,40 +31,58 @@
 
         $datos = json_decode($respuesta, true);
         $personajes = $datos["results"];
+        $paginas = $datos["info"]["pages"];
     ?>
-    <form action="" method="get">
-        <input type="text" name="page" id="page">
-        <select name="gender" id="gender">
+    <form action="" method="get" class="col-3">
+        <label for="page" class="form-label">Página:</label>
+        <select name="page" id="page" class="form-select">
+            <?php
+                for($j = 0; $j < $paginas; $j++) { ?>
+                    <option value="<?php echo $j+1 ?>"><?php echo $j+1 ?></option>
+                <?php } ?>
+        </select><br>
+        
+        <label for="quantity" class="form-label">Cantidad de personajes:</label>
+        <input type="text" name="quantity" id="quantity" class="form-control"><br>
+
+        <label for="gender" class="form-label">Género:</label>
+        <select name="gender" id="gender" class="form-select">
+            <option value="">Ambos</option>
             <option value="male">Masculino</option>
             <option value="female">Femenino</option>
-        </select>
-        <select name="species" id="species">
+        </select><br>
+
+        <label for="species" class="form-label">Especie:</label>
+        <select name="species" id="species" class="form-select">
+            <option value="">Ambos</option>
             <option value="human">Humano</option>
             <option value="alien">Alien</option>
-        </select>
-        <input type="submit" value="Mostrar">
-    </form>
+        </select><br>
 
-    <table>
-        <thead>
+        <input type="submit" value="Buscar" class="btn btn-info">
+    </form>
+    <br><br>
+    <table class="table table-bordered table-hover">
+        <thead class="thead-dark">
             <tr>
-                <th>Nombre</th>
-                <th>Género</th>
-                <th>Especie</th>
-                <th>Origen</th>
-                <th>Imagen</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Género</th>
+                <th scope="col">Especie</th>
+                <th scope="col">Origen</th>
+                <th scope="col">Imagen</th>
             </tr>
         </thead>
         <tbody>
             <?php
-                foreach($personajes as $personaje) { ?>
+                if ($cantidad > count($personajes)) $cantidad = count($personajes);
+                for($i = 0; $i < $cantidad; $i++) { ?>
                     <tr>
-                        <td><?php echo $personaje["name"]?></td>
-                        <td><?php echo $personaje["species"]?></td>
-                        <td><?php echo $personaje["gender"]?></td>
-                        <td><?php echo $personaje["origin"]["name"]?></td>
+                        <td><?php echo $personajes[$i]["name"]?></td>
+                        <td><?php echo $personajes[$i]["species"]?></td>
+                        <td><?php echo $personajes[$i]["gender"]?></td>
+                        <td><?php echo $personajes[$i]["origin"]["name"]?></td>
                         <td>
-                            <img width="100px" src="<?php echo $personaje["image"]?>">
+                            <img width="100px" src="<?php echo $personajes[$i]["image"]?>">
                         </td>
                     </tr>
                 <?php } ?>
