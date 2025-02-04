dd00bc086df923db5e1cacc64a9378484c0b472d
justmanuva <manuva2662004@gmail.com>
2025-02-04 21:43:19 +0100

    All ready before the APIs practice

---
+++ 07_apis/manejar-apis-externas/poke-api/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Top Anime</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <?php
+        error_reporting(E_ALL);
+        ini_set("display_errors", 1);
+    ?>
+</head>
+<body>
+    <?php
+        $apiUrl = "https://pokeapi.co/api/v2/pokedex/1/";
+        
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $apiUrl);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $respuesta = curl_exec($curl);
+        curl_close($curl);
+
+        $datos = json_decode($respuesta, true);
+        $pokemons = $datos["pokemon_entries"];
+    ?>
+    <table class="table">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">Nombre</th>
+                <!-- <th scope="col">Título</th>
+                <th scope="col">Nota</th>
+                <th scope="col">Imagen</th> -->
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                foreach($pokemons as $pokemon) { ?>
+                    <tr>
+                        <td scope="row"><?php echo $pokemon["pokemon_species"]["name"]?></td>
+                    </tr>
+                <?php } ?>
+        </tbody>
+    </table>
+</body>
+</html>
